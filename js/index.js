cf32769f3a194e43936c32f63609d7f98f18f2a5
iamyewon <rla357987@gmail.com>
2024-05-16 17:28:12 +0900

    20240516/17:27

--- js/index.js
+++ js/index.js
@@ -23,9 +23,20 @@ const newsSwiper = new Swiper(".news-swiper", {
 });
 
 
+
 // loop="true" slides-per-view="15" 
 //spaceBetween="30" initialSlide="10" centeredSlides="true" slideToClickedSlide="true" 
 
 
+document.querySelector('.btn-quick').addEventListener('click', () => {
+  const targetY = 855;
 
+  window.scrollTo({
+      top: targetY,
+      behavior: 'smooth' 
+  });
+});
 
+/**
+ * $('.swiper').hover(function() {    swiper.autoplay.stop();}, function(){    swiper.autoplay.start();});
+ */
