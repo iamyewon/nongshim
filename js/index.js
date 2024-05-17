f14032fc604a77004a2a19aa4078cfffe6fdd112
iamyewon <rla357987@gmail.com>
2024-05-17 13:55:05 +0900

    20240517/13:54
    제출전

--- js/index.js
+++ js/index.js
@@ -12,7 +12,6 @@ const jumbotronSwiper = new Swiper(".jumbotron-swiper", {
   },
 });
 
-
 const newsSwiper = new Swiper(".news-swiper", {
   loop: true,
   slidesPerView: 15,
@@ -21,11 +20,6 @@ const newsSwiper = new Swiper(".news-swiper", {
   centeredSlides: true,
   slideToClickedSlide: true,
 });
-
-
-
-// loop="true" slides-per-view="15" 
-//spaceBetween="30" initialSlide="10" centeredSlides="true" slideToClickedSlide="true" 
 
 
 document.querySelector('.btn-quick').addEventListener('click', () => {
@@ -37,6 +31,56 @@ document.querySelector('.btn-quick').addEventListener('click', () => {
   });
 });
 
-/**
- * $('.swiper').hover(function() {    swiper.autoplay.stop();}, function(){    swiper.autoplay.start();});
- */
+
+/* swiper 정지 / 시작 버튼 
+
+~~ .addEventListener('click', () => {
+  swiper.autoplay.stop();
+})
+
+~~ .addEventListener('click', () => {
+  swiper.autoplay.start();
+})
+*/
+
+const handleStorage = {
+  setStorage(name, value, exp) {
+    let date = new Date();
+    expireDate = date.setTime(date.getTime() + (exp * 24 * 60 * 60 * 1000));
+
+    const item = {
+      value: value, 
+      expire: expireDate,
+    }
+
+    localStorage.setItem(name, JSON.stringify(item));
+  },
+
+  getStorage(name) {
+    let currentDate = new Date();
+    currentDate = currentDate.setTime(currentDate.getTime());
+    return parseInt(localStorage.getItem(name)) > currentDate;
+  }
+};
+
+const popup = document.querySelector('.popup');
+const stopWatchingForToday = document.querySelector('.stop-watching-for-today');
+const popupCloseBtn = document.querySelector('.popup-close');
+
+stopWatchingForToday.addEventListener("click", () => {
+  handleStorage.setStorage("expiryDate", "", 1);
+  popup.classList.add('popup-hidden');
+})
+
+popupCloseBtn.addEventListener("click", () => {
+  popup.classList.add('popup-hidden');
+})
+
+
+if(handleStorage.getStorage("expiryDate")){
+  popup.classList.add('popup-hidden');
+}else{
+  popup.classList.remove('popup-hidden');
+}
+
+// 스토리지 미완
